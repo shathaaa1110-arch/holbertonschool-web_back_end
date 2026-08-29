5d2f7df26aed923fbd8dc11246d08a22322b54b9
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-08-29 16:39:07 +0300

    add task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a function for converting a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floating-point number."""
+    return str(n)
