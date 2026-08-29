9797915bcab809bb1fa1be29021fbd616849ff21
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-08-29 11:31:50 +0300

    add task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """This module provides a function for concatenating two strings."""
+
+
 def concat(a: str, b: str) -> str:
     """Return a concatenated string."""
     return a + b
