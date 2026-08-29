0b671c07f6d4020a6f9f48640f4a5980dfe21b10
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-08-29 16:45:16 +0300

    add task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module provides a function for summing a list of floats."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of a list of floating-point numbers."""
+    return sum(input_list)
